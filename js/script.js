43b8f0045f24e5d5637f6f6e624f162805bf71b6
Jordan J <jordanr2m@gmail.com>
2023-12-15 16:02:38 -0800

    Converted string to number.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let tipAmount;
 // Grab tip % from buttons
 tipButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-       tipAmount = e.target.innerText; // DO NOT use let here
+       tipAmount = Number(e.target.innerText); // Convert string to number
         // console.log(tipAmount);
     })
 });
@@ -21,22 +21,22 @@ const performCalculation = function () {
     let tip;
     let totalCost;
 
-    if (tipAmount == 10) {
+    if (tipAmount === 10) {
         tip = serviceCost * 0.1;
         totalCost = tip + serviceCost;
-    } else if (tipAmount == 15) {
+    } else if (tipAmount === 15) {
         tip = serviceCost * 0.15;
         totalCost = tip + serviceCost;
-    } else if (tipAmount == 18) {
+    } else if (tipAmount === 18) {
         tip = serviceCost * 0.18;
         totalCost = tip + serviceCost;
-    } else if (tipAmount == 20) {
+    } else if (tipAmount === 20) {
         tip = serviceCost * 0.2;
         totalCost = tip + serviceCost;
     }
 
     tipTotal.innerText = tip.toFixed(2);
-    totalPrice.innerText = totalCost.toFixed(2); // Adds 2 decimal places
+    totalPrice.innerText = totalCost.toFixed(2); // Adds or rounds to 2 decimal places
 };
 
 submitButton.addEventListener("click", (e) => {
